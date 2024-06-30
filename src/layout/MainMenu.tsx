875f478adbce798ca7ad874c71125ee4a63792e8
Robert Wan <wantunglok@gmail.com>
2024-06-30 10:33:56 +1000

    make menu responsive based on screen size

--- src/layout/MainMenu.tsx
+++ src/layout/MainMenu.tsx
@@ -3,14 +3,18 @@ import { Button } from "@mui/material";
 import styles from "./MainMenu.module.css";
 import routes from "../routes";
 
-export default function () {
+interface Props {
+  fullWidth: boolean;
+}
+
+export default function ({ fullWidth }: Props) {
   return (
     <>
       {routes.map((routes, index) => {
         return routes.children.map((route, index) => {
           return (
             <Link to={route.path} key={index}>
-              <Button variant="text" fullWidth className={styles.button}>
+              <Button variant="text" fullWidth={fullWidth} className={styles.button}>
                 {route.name}
               </Button>
             </Link>
