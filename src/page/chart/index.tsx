dfe97494e1feb1392e75d42e9b25dfffc8c6b7f6
Robert Wan <wantunglok@gmail.com>
2024-07-01 10:14:02 +1000

    add functions to capture date range

--- src/page/chart/index.tsx
+++ src/page/chart/index.tsx
@@ -1,8 +1,6 @@
 import { Dataset } from "../../common/Types";
 import LineChart from "./LineChart";
 
-const year = 2024;
-
 const monthNames = [
   "Jan",
   "Feb",
@@ -18,47 +16,86 @@ const monthNames = [
   "Dec",
 ];
 
-function getDates() {
+function getNumberOfMonths(startDate: Date, endDate: Date) {
+  const yearMonthSet: Set<string> = new Set();
+  while (startDate <= endDate) {
+    yearMonthSet.add(
+      startDate.getFullYear() + `${startDate.getMonth() + 1}`.padStart(2, "0")
+    );
+    startDate.setDate(startDate.getDate() + 1);
+  }
+  return yearMonthSet.size;
+}
+
+function getDatesBetweenDateString(
+  startDateString: string,
+  endDateString: string
+) {
   const dates = [];
-  for (var month = 1; month <= 12; month++) {
-    const numberOfDays = new Date(year, month, 0).getDate();
-    for (var day = 1; day <= numberOfDays; day++) {
-      dates.push(day + " " + monthNames[month - 1] + " " + year);
-    }
+  const startDate = new Date(startDateString);
+  const endDate = new Date(endDateString);
+  while (startDate <= endDate) {
+    dates.push(new Date(startDate));
+    startDate.setDate(startDate.getDate() + 1);
   }
   return dates;
 }
 
-function getValues() {
+function getValues(dates: Date[]) {
   const values = [];
-  for (var month = 1; month <= 12; month++) {
-    const numberOfDays = new Date(year, month, 0).getDate();
-    for (var day = 1; day <= numberOfDays; day++) {
+  if (dates.length > 0) {
+    const startDate = dates[0];
+    const endDate = dates[dates.length - 1];
+    const numberOfDays =
+      (endDate.getTime() - startDate.getTime()) / (1000 * 3600 * 24);
+    for (var i = 0; i < numberOfDays; i++) {
       values.push(Math.floor(Math.random() * 100));
     }
   }
   return values;
 }
 
-const dataset: Dataset = {
-  title: "Peak Efficiency",
-  series: [
-    {
-      name: "Line 1",
-      colour: "pink",
-      data: getValues(),
-    },
-    // {
-    //   name: "Line 2",
-    //   colour: "silver",
-    //   data: getValues(),
-    // },
-  ],
-  x: getDates(),
-  compareEnabled: true,
-};
+function getDateString(date: Date) {
+  const day = date.getDate();
+  const monthName = monthNames[date.getMonth()];
+  const year = date.getFullYear();
+  return day + " " + monthName + " " + year;
+}
+
+function getDateStrings(dates: Date[]) {
+  const dateStrings = [];
+  if (dates.length > 0) {
+    const startDate = dates[0];
+    const endDate = dates[dates.length - 1];
+    while (startDate <= endDate) {
+      dateStrings.push(getDateString(startDate));
+      startDate.setDate(startDate.getDate() + 1);
+    }
+  }
+  return dateStrings;
+}
 
 export default function () {
+  const dates = getDatesBetweenDateString("2024-06-05", "2025-07-12");
+  const firstDateString = dates.length > 0 ? getDateString(dates[0]) : "";
+  const dataset: Dataset = {
+    title: "Peak Efficiency",
+    series: [
+      {
+        name: "Line 1",
+        colour: "pink",
+        data: getValues(dates),
+      },
+      // {
+      //   name: "Line 2",
+      //   colour: "silver",
+      //   data: getValues(),
+      // },
+    ],
+    x: getDateStrings(dates),
+    compareEnabled: true,
+  };
+
   return (
     <>
       <div style={{ height: "40px" }}>Chart</div>
@@ -69,15 +106,19 @@ export default function () {
             var label = "";
             if (value != null) {
               const slices = value.split(" ");
-              label = slices[0] == "1" ? slices[1] + " " + slices[2] : "";
+              label =
+                value == firstDateString || slices[0] == "1"
+                  ? slices[1] + " " + slices[2]
+                  : "";
             }
             return label;
           }}
           chartWidth={function (width: number) {
-            const numberOfMonths = 6;
+            const numberOfMonthsToDisplay = 6;
             const scalingFactor = 4392;
             return (
-              ((width / dataset.x.length) * scalingFactor) / numberOfMonths
+              ((width / dataset.x.length) * scalingFactor) /
+              numberOfMonthsToDisplay
             );
           }}
         />
