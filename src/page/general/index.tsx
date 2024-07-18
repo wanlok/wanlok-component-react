de02a96639b4dc7ac26294c70b755da24a9b7d6c
Robert Wan <wantunglok@gmail.com>
2024-07-18 20:46:03 +1000

    test background filter

--- src/page/general/index.tsx
+++ src/page/general/index.tsx
@@ -4,11 +4,11 @@ import AlertChart from "./AlertChart";
 import API from "../../common/API";
 
 export default function () {
-    const data = useLoaderData();
+    // const data = useLoaderData();
     const { ref, width, height } = getDimension();
 
     console.log(height);
-    console.log(data);
+    // console.log(data);
 
     return (
         // <div ref={ref} style={{ width: "100%", backgroundColor: "green" }}>
@@ -17,7 +17,14 @@ export default function () {
         //     <p>Height: {height}px</p>
         // </div>
 
-        <div ref={ref} style={{ height: "100%", backgroundColor: "green" }}>
+        <div
+            ref={ref}
+            style={{
+                height: "100%",
+                backgroundColor: "green",
+                position: "relative"
+            }}
+        >
             {/* <div ref={ref}>
                 <AlertChart
                     dataset={{}}
@@ -30,12 +37,30 @@ export default function () {
                     height={height * 0.4}
                 />
             </div> */}
+            <img
+                src={require("../../assets/images/dummy.jpg")}
+                style={{ position: "absolute", top: 0, left: 0 }}
+            />
+            <div
+                style={{
+                    padding: 40,
+                    backgroundColor: "rgba(255, 0, 0, 0.4)",
+                    display: "inline-block",
+                    position: "absolute",
+                    top: 100,
+                    left: 100,
+                    backdropFilter: "blur(8px)"
+                }}
+            >
+                Hello World
+            </div>
         </div>
     );
 }
 
 export function loader() {
-    const jwt =
-    "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiJ3YW5sb2siLCJleHAiOjE3MjAwODc1MTl9.tcEhS4lM5gEvu9nZAbXaKPzhW_dmzSkQXZNqwlay3fQ";
-    return API.get_gwin(jwt);
+    // const jwt =
+    // "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiJ3YW5sb2siLCJleHAiOjE3MjAwODc1MTl9.tcEhS4lM5gEvu9nZAbXaKPzhW_dmzSkQXZNqwlay3fQ";
+    // return API.get_gwin(jwt);
+    return "";
 }
