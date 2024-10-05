b738004cf029315413fac61fc36b2d7f82a930d9
Robert Wan <wantunglok@gmail.com>
2024-10-05 12:23:07 +1000

    update cursor style on building

--- src/page/hong-kong-map/ArcGISMap.tsx
+++ src/page/hong-kong-map/ArcGISMap.tsx
@@ -1,20 +1,14 @@
-import { Dispatch, SetStateAction, useEffect, useRef, useState } from "react";
+import { useEffect, useState } from "react";
 import Map from "@arcgis/core/Map";
 import SceneLayer from "@arcgis/core/layers/SceneLayer";
 import SceneView from "@arcgis/core/views/SceneView";
-// import MapView from '@arcgis/core/views/MapView';
 import FeatureLayer from "@arcgis/core/layers/FeatureLayer";
-import Extent from "@arcgis/core/geometry/Extent";
-
 import UniqueValueRenderer from "@arcgis/core/renderers/UniqueValueRenderer";
 import SimpleFillSymbol from "@arcgis/core/symbols/SimpleFillSymbol";
 import SimpleLineSymbol from "@arcgis/core/symbols/SimpleLineSymbol";
 import MeshSymbol3D from "@arcgis/core/symbols/MeshSymbol3D";
 import FillSymbol3DLayer from "@arcgis/core/symbols/FillSymbol3DLayer";
-
 import "./ArcGISMap.css";
-// import './Tooltip.css';
-import { useNavigate } from "react-router-dom";
 
 const initialLocation = {
     center: [114.1095, 22.345],
@@ -46,6 +40,18 @@ function getUniqueValueInfos(buildingIds: number[]) {
     return uniqueValueInfos;
 }
 
+export function isBuilding(response: any) {
+    var building = false;
+    for (var i = 0; i < response.results.length; i++) {
+        const attributes = response.results[i].graphic.attributes;
+        if (attributes !== null && attributes["BUILDINGID"] !== null) {
+            building = true;
+            break;
+        }
+    }
+    return building;
+}
+
 export function getBuildingIds(response: any): number[] {
     const buildingIds = [];
     for (var i = 0; i < response.results.length; i++) {
@@ -57,70 +63,6 @@ export function getBuildingIds(response: any): number[] {
     return buildingIds;
 }
 
-// function getSelectedSiteIndex(response: any, selectedDistrictIndex: number) {
-//     var selectedSiteIndex = -1;
-//     if (response.results.length > 0 && selectedDistrictIndex > -1) {
-//         for (var i = 0; i < response.results.length; i++) {
-//             const attributes = response.results[i].graphic.attributes;
-//             if (attributes != null && attributes['BUILDINGID'] != null) {
-//                 for (var j = 0; j < Districts[selectedDistrictIndex].sites.length; j++) {
-//                     for (var k = 0; k < Districts[selectedDistrictIndex].sites[j].building_ids.length; k++) {
-//                         if (Districts[selectedDistrictIndex].sites[j].building_ids[k] === attributes['BUILDINGID']) {
-//                             selectedSiteIndex = j;
-//                             break;
-//                         }
-//                     }
-//                     if (selectedSiteIndex > -1) {
-//                         break;
-//                     }
-//                 }
-//             }
-//             if (selectedSiteIndex > -1) {
-//                 break;
-//             }
-//         }
-//     }
-//     return selectedSiteIndex;
-// }
-
-// function updateBuildingTooltipAndCursor(
-//     sceneView: SceneView,
-//     sceneLayer: SceneLayer,
-//     response: any,
-//     selectedDistrictIndex: number,
-//     selectedSiteIndex: number,
-//     setSelectedSiteIndex: Dispatch<SetStateAction<number>>
-// ) {
-//     const result = response.results.filter(function (result: any) {
-//         return result.graphic != null && result.graphic.layer === sceneLayer;
-//     })[0];
-//     var siteIndex = -1;
-//     if (result != null) {
-//         for (var i = 0; i < Districts[selectedDistrictIndex].sites.length; i++) {
-//             for (var j = 0; j < Districts[selectedDistrictIndex].sites[i].building_ids.length; j++) {
-//                 if (Districts[selectedDistrictIndex].sites[i].building_ids[j] === result.graphic.attributes['BUILDINGID']) {
-//                     siteIndex = i;
-//                     break;
-//                 }
-//             }
-//             if (siteIndex > -1) {
-//                 break;
-//             }
-//         }
-//     }
-//     if (siteIndex != selectedSiteIndex) {
-//         setSelectedSiteIndex(siteIndex);
-//     }
-//     Tooltip.clear(sceneView.container);
-//     if (siteIndex > -1) {
-//         const screenPoint = sceneView.toScreen(result.mapPoint);
-//         Tooltip.append(sceneView.container, screenPoint.x, screenPoint.y, 'Site ' + (siteIndex + 1));
-//         sceneView.container.style.cursor = 'pointer';
-//     } else {
-//         sceneView.container.style.cursor = 'default';
-//     }
-// }
-
 function ArcGISMap({
     height,
     buildingIds,
@@ -130,10 +72,9 @@ function ArcGISMap({
     buildingIds: number[];
     onClick: (response: any) => void;
 }) {
-    const navigate = useNavigate();
     const [sceneView, setSceneView] = useState<SceneView>();
     const [sceneLayer, setSceneLayer] = useState<SceneLayer>();
-    
+
     useEffect(() => {
         const map = new Map({
             basemap: "dark-gray-vector",
@@ -175,33 +116,23 @@ function ArcGISMap({
         });
         map.add(featureLayer, 0);
 
+        sceneView.on("double-click", function (event) {
+            event.stopPropagation();
+        });
+
         sceneView.on("click", function (event) {
             sceneView?.hitTest(event).then(function (response) {
                 onClick(response);
             });
         });
 
-        sceneView.on("double-click", function (event) {
-            event.stopPropagation();
+        sceneView.on("pointer-move", function (event) {
+            sceneView.hitTest(event).then(function (response) {
+                sceneView.container.style.cursor = isBuilding(response)
+                    ? "pointer"
+                    : "default";
+            });
         });
-
-        // sceneView.on('pointer-move', function (event) {
-        //     sceneView.hitTest(event).then(function (response) {
-        //         sceneView.container.style.cursor = 'default';
-        //         if (sceneView.zoom >= 15 - 0.5) {
-        //             updateBuildingTooltipAndCursor(
-        //                 sceneView,
-        //                 sceneLayer,
-        //                 response,
-        //                 selectedDistrictIndexRef.current - 1,
-        //                 selectedSiteIndexRef.current,
-        //                 setSelectedSiteIndex
-        //             );
-        //         } else {
-        //             updateDistrictCursor(sceneView, featureLayer, response);
-        //         }
-        //     });
-        // });
 
         return () => {
             featureLayer.destroy();
