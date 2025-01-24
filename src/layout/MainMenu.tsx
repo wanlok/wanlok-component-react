732ae1a560e0bcadafe1b496a9e51a9f4e5c0e35
Robert Wan <wantunglok@gmail.com>
2025-01-24 20:43:00 +1100

    update selected page highlight colour

--- src/layout/MainMenu.tsx
+++ src/layout/MainMenu.tsx
@@ -1,5 +1,5 @@
 import { Link } from "react-router-dom";
-import { Button } from "@mui/material";
+import { Button, useTheme } from "@mui/material";
 import styles from "./MainMenu.module.css";
 import routes from "../routes";
 import { useState } from "react";
@@ -11,7 +11,9 @@ export default function ({
   buttonHeight: number;
   fullWidth: boolean;
 }) {
+  const { palette } = useTheme();
   const [path, setPath] = useState<string>("/");
+
   return (
     <>
       {routes.map((routes, index) => {
@@ -20,16 +22,21 @@ export default function ({
             <Link to={route.path} key={index}>
               <Button
                 color="primary"
-                variant={path === route.path ? "contained" : "text"}
+                variant="contained"
                 disableElevation
                 fullWidth={fullWidth}
                 className={styles.button}
-                sx={{
-                  height: buttonHeight,
-                  borderRadius: 0,
-                  p: 2,
-                  textTransform: "none"
-                }}
+                sx={[
+                  {
+                    height: buttonHeight,
+                    borderRadius: 0,
+                    p: 2,
+                    textTransform: "none"
+                  },
+                  path === route.path
+                    ? { backgroundColor: palette.primary.dark }
+                    : {}
+                ]}
                 onClick={() => {
                   setPath(route.path);
                 }}
