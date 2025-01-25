a97b24120b488e4e12cdd5c0bbc4b2a77afa7044
Robert Wan <wantunglok@gmail.com>
2025-01-26 10:05:10 +1100

    add mac deploy script

---
+++ deploy.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+BUILD_DIRECTORY_PATH="/Users/wanlok/Files/Projects/wanlok-component-react/build"
+GITHUB_PAGES_DIRECTORY_PATH="/Users/wanlok/Files/Projects/wanlok.github.io"
+
+rm -rf "$BUILD_DIRECTORY_PATH"
+
+find "$GITHUB_PAGES_DIRECTORY_PATH" -mindepth 1 -not -path "$GITHUB_PAGES_DIRECTORY_PATH/.git" -not -path "$GITHUB_PAGES_DIRECTORY_PATH/.git/*" -exec rm -rf {} +
+
+npm run build && {
+  cp -r "$BUILD_DIRECTORY_PATH/." "$GITHUB_PAGES_DIRECTORY_PATH"
+  rm -rf $BUILD_DIRECTORY_PATH
+  cd $GITHUB_PAGES_DIRECTORY_PATH
+  git add .
+  git commit -m "commit"
+  git push
+}
