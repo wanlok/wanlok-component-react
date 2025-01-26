c93f77d8314f2a0fe0dff99f048a82124d109cd0
Robert Wan <wantunglok@gmail.com>
2025-01-27 10:08:03 +1100

    update deployment script for macos

--- deploy.sh
+++ deploy.sh
@@ -1,17 +1,30 @@
 #!/bin/bash
 
-BUILD_DIRECTORY_PATH="/Users/wanlok/Files/Projects/wanlok-component-react/build"
-GITHUB_PAGES_DIRECTORY_PATH="/Users/wanlok/Files/Projects/wanlok.github.io"
+REPOSITORY_PATH="/Users/wanlok/Files/Projects/wanlok-component-react"
+DEPLOY_PATH="/Users/wanlok/Files/Projects/wanlok.github.io"
 
-rm -rf "$BUILD_DIRECTORY_PATH"
+BUILD_PATH=$REPOSITORY_PATH/build
 
-find "$GITHUB_PAGES_DIRECTORY_PATH" -mindepth 1 -not -path "$GITHUB_PAGES_DIRECTORY_PATH/.git" -not -path "$GITHUB_PAGES_DIRECTORY_PATH/.git/*" -exec rm -rf {} +
+cd $DEPLOY_PATH
+
+git pull
+
+find $DEPLOY_PATH -mindepth 1 -not -path $DEPLOY_PATH/.git -not -path "$DEPLOY_PATH/.git/*" -exec rm -rf {} +
+
+cd $REPOSITORY_PATH
+
+rm -rf $BUILD_PATH
 
 npm run build && {
-  cp -r "$BUILD_DIRECTORY_PATH/." "$GITHUB_PAGES_DIRECTORY_PATH"
-  rm -rf "$BUILD_DIRECTORY_PATH"
-  cd $GITHUB_PAGES_DIRECTORY_PATH
+  cp -r $BUILD_PATH/. $DEPLOY_PATH
+
+  cd $DEPLOY_PATH
+
   git add .
   git commit -m "commit"
   git push
+
+  cd $REPOSITORY_PATH
+
+  rm -rf $BUILD_PATH
 }
