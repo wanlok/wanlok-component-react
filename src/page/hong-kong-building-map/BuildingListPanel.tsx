d2e7fa8643d93e32672d8d38a9386e6745a4bc48
Robert Wan <wantunglok@gmail.com>
2025-01-27 16:18:57 +1100

    add building save feature

--- src/page/hong-kong-building-map/BuildingListPanel.tsx
+++ src/page/hong-kong-building-map/BuildingListPanel.tsx
@@ -12,20 +12,34 @@ function BuildingListPanel({
   onLocateButtonClick: (building: Building) => void;
   onDeleteButtonClick: (building: Building) => void;
 }) {
+  const save = () => {
+    const jsonString = JSON.stringify(buildings, null, 2);
+    const blob = new Blob([jsonString], { type: "application/json" });
+    const link = document.createElement("a");
+    link.href = URL.createObjectURL(blob);
+    link.download = "data.json";
+    document.body.appendChild(link);
+    link.click();
+    document.body.removeChild(link);
+  };
+
   return (
     <Grid container className={classes.container}>
       <Grid item xs={12} sm={12} md={12}>
         <Typography variant="h5" component="h2" sx={{ mb: 2 }}>
           Building List ({buildings.length})
         </Typography>
-        <PrimaryButton>Export</PrimaryButton>
+        <ButtonGroup disableElevation variant="contained" aria-label="Basic button group" fullWidth>
+          <PrimaryButton>Load</PrimaryButton>
+          <PrimaryButton onClick={save}>Save</PrimaryButton>
+        </ButtonGroup>
         {buildings.map((building, index) => (
           <Card elevation={0} sx={{ mt: 2 }}>
             <CardContent>
               <Typography>Building {index + 1}</Typography>
               {/* <Typography>{building.buildingIdList}</Typography> */}
             </CardContent>
-            <ButtonGroup variant="contained" aria-label="Basic button group" fullWidth>
+            <ButtonGroup disableElevation variant="contained" aria-label="Basic button group" fullWidth>
               <PrimaryButton onClick={() => onLocateButtonClick(building)}>Locate</PrimaryButton>
               <PrimaryButton onClick={() => onDeleteButtonClick(building)}>Delete</PrimaryButton>
             </ButtonGroup>
