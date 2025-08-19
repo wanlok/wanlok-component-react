083ac8bd05e3897a5bdf24b05c96548eb87c777d
Robert Wan <wantunglok@gmail.com>
2025-08-19 21:25:30 +1000

    add export button and update styles

--- src/components/TextInputForm.tsx
+++ src/components/TextInputForm.tsx
@@ -1,7 +1,9 @@
 import { Stack } from "@mui/material";
-import { useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import { TextInput } from "./TextInput";
 import { WButton } from "./WButton";
+
+const buttonWidth = 80;
 
 export const TextInputForm = ({
   placeholder,
@@ -11,6 +13,15 @@ export const TextInputForm = ({
   onSubmitClick: (text: string) => void;
 }) => {
   const [text, setText] = useState<string>("");
+  const [buttonHeight, setButtonHeight] = useState<number>(0);
+
+  const stackRef = useRef<HTMLDivElement>(null);
+
+  useEffect(() => {
+    if (stackRef.current) {
+      setButtonHeight(stackRef.current.offsetHeight);
+    }
+  }, []);
 
   const onClick = async () => {
     if (text && text.trim().length > 0) {
@@ -20,16 +31,16 @@ export const TextInputForm = ({
   };
 
   return (
-    <Stack sx={{ flexDirection: "row", backgroundColor: "#EEEEEE" }}>
+    <Stack ref={stackRef} sx={{ flexDirection: "row", backgroundColor: "#EEEEEE" }}>
       <Stack sx={{ flex: 1, p: 1 }}>
         <TextInput placeholder={placeholder} value={text} onChange={(value) => setText(value)} hideHelperText={true} />
       </Stack>
       <Stack sx={{ flexDirection: "row", gap: "1px" }}>
-        <WButton onClick={onClick} sx={{ height: "100%" }}>
-          Save
-        </WButton>{" "}
-        <WButton onClick={onClick} sx={{ height: "100%" }}>
-          Button 2
+        <WButton onClick={onClick} sx={{ width: buttonWidth, height: buttonHeight }}>
+          Add
+        </WButton>
+        <WButton onClick={onClick} sx={{ width: buttonWidth, height: buttonHeight }}>
+          Export
         </WButton>
       </Stack>
     </Stack>
