d05035b3ed47eb3cb6ba563c1bc0c25b7c2b421d
Robert Wan <wantunglok@gmail.com>
2025-08-21 17:40:54 +1000

    update layout

--- src/layouts/Main.tsx
+++ src/layouts/Main.tsx
@@ -1,34 +1,51 @@
 import { Stack, useMediaQuery, useTheme } from "@mui/material";
-import { Outlet } from "react-router-dom";
-import { MainMenu } from "./MainMenu";
+import { Link, matchPath, Outlet, useLocation } from "react-router-dom";
 import { useWindowDimensions } from "../common/useWindowDimension";
+import { routes } from "../configs/routes";
+import { WButton } from "../components/WButton";
 
 export const Main = () => {
-  const { breakpoints } = useTheme();
+  const { pathname } = useLocation();
+  const { breakpoints, palette } = useTheme();
+  const { height } = useWindowDimensions();
   const mobile = useMediaQuery(breakpoints.down("md"));
-  const height = useWindowDimensions().height;
   const buttonHeight = 56;
   const contentHeight = height - (mobile ? buttonHeight : 0);
+
+  const mainRoute = routes.find((route) => route.element?.type === Main);
+  const filteredRoutes = mainRoute?.children?.filter((child) => child.name !== undefined) ?? [];
+
   return (
-    <Stack sx={{ flexDirection: mobile ? "column" : "row", height: "100vh" }}>
+    <Stack sx={{ flexDirection: mobile ? "column" : "row", height }}>
       <Stack
-        sx={[
-          { backgroundColor: "primary.main" },
-          mobile
-            ? {
-                flexDirection: "row",
-                height: buttonHeight,
-                overflowX: "auto"
-              }
-            : {
-                flexDirection: "column",
-                width: "320px"
-              }
-        ]}
+        sx={{
+          backgroundColor: "primary.main",
+          height: mobile ? buttonHeight : height,
+          flexDirection: mobile ? "row" : "column",
+          overflowX: "auto"
+        }}
       >
-        <MainMenu buttonHeight={buttonHeight} fullWidth={!mobile} />
+        {filteredRoutes.map((route, index) => (
+          <Link to={route.path} key={index}>
+            <WButton
+              fullWidth={!mobile}
+              sx={[
+                {
+                  height: buttonHeight,
+                  textTransform: "none",
+                  borderRadius: 0
+                },
+                matchPath({ path: route.path, end: route.path === "/" }, pathname)
+                  ? { backgroundColor: palette.primary.dark }
+                  : {}
+              ]}
+            >
+              {route.name}
+            </WButton>
+          </Link>
+        ))}
       </Stack>
-      <Stack sx={[{ flex: 1 }, mobile ? { height: "1%" } : {}]}>
+      <Stack sx={[{ flex: 1, height: contentHeight }]}>
         <Outlet context={[contentHeight]} />
       </Stack>
     </Stack>
