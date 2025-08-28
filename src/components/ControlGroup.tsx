2b0794ef10b762f4e8225ddf53ccbb7498eb34dc
Robert Wan <wantunglok@gmail.com>
2025-08-28 23:32:19 +1000

    make buttons transparent

--- src/components/ControlGroup.tsx
+++ src/components/ControlGroup.tsx
@@ -1,4 +1,4 @@
-import { Stack } from "@mui/material";
+import { alpha, Stack, useTheme } from "@mui/material";
 import { WIconButton } from "./WButton";
 import LeftWhiteIcon from "../assets/images/icons/left_white.png";
 import RightWhiteIcon from "../assets/images/icons/right_white.png";
@@ -13,25 +13,26 @@ export const ControlGroup = ({
   onRightButtonClick: () => void;
   onDeleteButtonClick: () => void;
 }) => {
+  const theme = useTheme();
   return (
     <Stack sx={{ flexDirection: "column", gap: "1px", position: "absolute", top: 0, right: 0 }}>
       <WIconButton
         icon={CrossWhiteIcon}
         iconSize={16}
         onClick={onDeleteButtonClick}
-        sx={{ backgroundColor: "common.black" }}
+        sx={{ backgroundColor: alpha(theme.palette.common.black, 0.6) }}
       />
       <WIconButton
         icon={RightWhiteIcon}
         iconSize={16}
         onClick={onLeftButtonClick}
-        sx={{ backgroundColor: "common.black" }}
+        sx={{ backgroundColor: alpha(theme.palette.common.black, 0.6) }}
       />
       <WIconButton
         icon={LeftWhiteIcon}
         iconSize={16}
         onClick={onRightButtonClick}
-        sx={{ backgroundColor: "common.black" }}
+        sx={{ backgroundColor: alpha(theme.palette.common.black, 0.6) }}
       />
     </Stack>
   );
