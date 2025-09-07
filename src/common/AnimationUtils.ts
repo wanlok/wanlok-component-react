51831395e89e6881415331ac964a8291b4df91c4
Robert Wan <wantunglok@gmail.com>
2025-09-07 19:29:55 +1000

    seperate animation loop

--- src/common/AnimationUtils.ts
+++ src/common/AnimationUtils.ts
@@ -1,25 +1,14 @@
-export const startAnimationLoop = (fps: number, callback: () => Promise<void>) => {
+export const startAnimationLoop = (framePerSecond: number, callback: () => void) => {
   let handle: number;
-  let lastTime = performance.now();
-  let running = false;
-  const frameDuration = 1000 / fps;
-
-  const loop = async (time: number) => {
-    const delta = time - lastTime;
-
-    if (!running && delta >= frameDuration) {
+  let lastTime = 0;
+  const frameDuration = 1000 / framePerSecond;
+  const loop = (time: number) => {
+    if (time - lastTime >= frameDuration) {
       lastTime = time;
-      running = true;
-      try {
-        await callback();
-      } finally {
-        running = false;
-      }
+      callback();
     }
-
     handle = requestAnimationFrame(loop);
   };
-
   handle = requestAnimationFrame(loop);
   return () => cancelAnimationFrame(handle);
 };
