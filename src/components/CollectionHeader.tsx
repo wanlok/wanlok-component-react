8bc57d65bd16d63a942c778f98c1717f4fb613ab
Robert Wan <wantunglok@gmail.com>
2025-09-12 23:31:35 +1000

    hide controls on mobile

--- src/components/CollectionHeader.tsx
+++ src/components/CollectionHeader.tsx
@@ -4,13 +4,13 @@ import { ReactNode } from "react";
 export const CollectionHeader = ({ top, bottom }: { top: ReactNode; bottom: ReactNode }) => {
   const { breakpoints } = useTheme();
   const mobile = useMediaQuery(breakpoints.down("md"));
-  return (
+  return mobile ? (
+    <></>
+  ) : (
     <Stack sx={mobile ? {} : { height: 100 }}>
-      {!mobile && (
-        <Stack sx={{ flex: 1, justifyContent: "center", px: 1, backgroundColor: "background.default" }}>
-          <Stack sx={{ flexDirection: "row", alignItems: "center", gap: 1 }}>{top}</Stack>
-        </Stack>
-      )}
+      <Stack sx={{ flex: 1, justifyContent: "center", px: 1, backgroundColor: "background.default" }}>
+        <Stack sx={{ flexDirection: "row", alignItems: "center", gap: 1 }}>{top}</Stack>
+      </Stack>
       <Stack sx={{ flexDirection: "row", alignItems: "center", gap: "1px", backgroundColor: "background.default" }}>
         {bottom}
       </Stack>
