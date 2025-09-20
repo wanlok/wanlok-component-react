989e1625a48177d1c5f1408b5c4ba53f97890189
Robert Wan <wantunglok@gmail.com>
2025-09-21 09:25:58 +1000

    refactor kanban column structure

--- src/pages/kanban/KanbanLayout.tsx
+++ src/pages/kanban/KanbanLayout.tsx
@@ -1,37 +1,37 @@
 import { Card, CardContent, Divider, Stack, Typography } from "@mui/material";
-import { ReactNode, RefObject, useRef, useState } from "react";
+import { RefObject, useRef, useState } from "react";
 import Draggable from "react-draggable";
 
-const threshold = 0.25;
+const xThreshold = 0.25;
+// const yThreshold = 1;
 
-const getColumnWidth = (stackRef: RefObject<HTMLDivElement>) => {
+const getColumnWidthAndHeight = (stackRef: RefObject<HTMLDivElement>) => {
   const rect = stackRef.current?.getBoundingClientRect();
-  return rect ? rect.width : 0;
+  return rect ? { width: rect.width, height: rect.height } : { width: 0, height: 0 };
 };
 
-export const KanbanCard = ({
+const KanbanCard = ({
   stackRef,
-  onIndexChange,
-  children
+  item,
+  onIndexChange
 }: {
   stackRef: RefObject<HTMLDivElement>;
-  onIndexChange: (i: number) => void;
-  children: ReactNode;
+  item: string;
+  onIndexChange: (item: string, i: number) => void;
 }) => {
   const [pos, setPos] = useState({ x: 0, y: 0 });
   return (
     <Draggable
       handle=".drag-handle"
       position={pos}
-      onStop={(_, { x }) => {
-        const percentage = (x + 16) / getColumnWidth(stackRef);
-        const i =
-          Math.abs(percentage) > threshold ? Math.sign(percentage) * Math.ceil(Math.abs(percentage) - threshold) : 0;
-        console.log(i);
+      onStop={(_, { x, y }) => {
+        const { width } = getColumnWidthAndHeight(stackRef);
+        const xDiff = (x + 16) / width;
+        const i = Math.abs(xDiff) > xThreshold ? Math.sign(xDiff) * Math.ceil(Math.abs(xDiff) - xThreshold) : 0;
         if (i === 0) {
           setPos({ x: 0, y: 0 });
         } else {
-          onIndexChange(i);
+          onIndexChange(item, i);
         }
       }}
     >
@@ -44,49 +44,55 @@ export const KanbanCard = ({
         }}
         className="drag-handle"
       >
-        <CardContent>{children}</CardContent>
+        <CardContent>
+          <Typography>{item}</Typography>
+        </CardContent>
       </Card>
     </Draggable>
+  );
+};
+
+const KanbanColumn = ({
+  list,
+  onIndexChange
+}: {
+  list: string[];
+  onIndexChange: (item: string, i: number) => void;
+}) => {
+  const stackRef = useRef<HTMLDivElement>(null);
+  return (
+    <Stack ref={stackRef} sx={{ flex: 1, p: 2 }}>
+      {list.map((item) => (
+        <KanbanCard stackRef={stackRef} key={item} item={item} onIndexChange={onIndexChange} />
+      ))}
+    </Stack>
   );
 };
 
 export const KanbanLayout = () => {
   const [data, setData] = useState<
     {
-      ref: RefObject<HTMLDivElement>;
       list: string[];
     }[]
-  >([
-    { ref: useRef<HTMLDivElement>(null), list: ["AAAAA"] },
-    { ref: useRef<HTMLDivElement>(null), list: ["BBBBB", "CCCCC"] },
-    { ref: useRef<HTMLDivElement>(null), list: ["DDDDD", "EEEEE"] },
-    { ref: useRef<HTMLDivElement>(null), list: ["FFFFF"] }
-  ]);
+  >([{ list: ["AAAAA"] }, { list: ["BBBBB", "CCCCC"] }, { list: ["DDDDD", "EEEEE"] }, { list: ["FFFFF"] }]);
 
   return (
     <Stack sx={{ flex: 1, flexDirection: "row" }}>
-      {data.map(({ ref, list }, i) => (
+      {data.map(({ list }, i) => (
         <>
           {i !== 0 && <Divider orientation="vertical" />}
-          <Stack ref={ref} sx={{ flex: 1, p: 2 }}>
-            {list.map((item) => (
-              <KanbanCard
-                stackRef={ref}
-                key={item}
-                onIndexChange={(j) => {
-                  setData((prev) => {
-                    const newData = [...prev];
-                    newData[i].list = newData[i].list.filter((i) => i !== item);
-                    const targetIndex = (i + j + newData.length) % newData.length;
-                    newData[targetIndex].list = [...newData[targetIndex].list, item];
-                    return newData;
-                  });
-                }}
-              >
-                <Typography>{item}</Typography>
-              </KanbanCard>
-            ))}
-          </Stack>
+          <KanbanColumn
+            list={list}
+            onIndexChange={(item, j) => {
+              setData((prev) => {
+                const newData = [...prev];
+                newData[i].list = newData[i].list.filter((i) => i !== item);
+                const k = (i + j + newData.length) % newData.length;
+                newData[k].list = [...newData[k].list, item];
+                return newData;
+              });
+            }}
+          />
         </>
       ))}
     </Stack>
