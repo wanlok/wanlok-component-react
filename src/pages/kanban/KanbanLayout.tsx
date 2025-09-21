c9137a0c2fda7a529423bbb983f826e01fe3c965
Robert Wan <wantunglok@gmail.com>
2025-09-21 22:26:31 +1000

    refactor column sorting logic

--- src/pages/kanban/KanbanLayout.tsx
+++ src/pages/kanban/KanbanLayout.tsx
@@ -9,7 +9,9 @@ const data = [
   { name: "column-4", list: ["FFFFF"] }
 ];
 
-const xThreshold = 0.25;
+const padding = 16;
+
+const xThreshold = 0.8;
 // const yThreshold = 1;
 
 const getColumnWidthAndHeight = (stackRef: RefObject<HTMLDivElement>) => {
@@ -17,30 +19,47 @@ const getColumnWidthAndHeight = (stackRef: RefObject<HTMLDivElement>) => {
   return rect ? { width: rect.width, height: rect.height } : { width: 0, height: 0 };
 };
 
+const getColumnOffset = (stackRef: RefObject<HTMLDivElement>, x: number) => {
+  const ratio = (x + padding) / getColumnWidthAndHeight(stackRef).width;
+  return Math.abs(ratio) > xThreshold ? Math.sign(ratio) * Math.ceil(Math.abs(ratio) - xThreshold) : 0;
+};
+
+const getRowOffset = (stackRef: RefObject<HTMLDivElement>, node: HTMLElement) => {
+  let offset = 0;
+  const children = stackRef.current?.children ?? [];
+  for (let i = 0; i < children.length; i++) {
+    if (children[i] !== node) {
+      if (node.getBoundingClientRect().y >= children[i].getBoundingClientRect().y) {
+        offset = i;
+      }
+    }
+  }
+  return offset;
+};
+
 const KanbanCard = ({
   stackRef,
   item,
-  onIndexChange
+  onColumnChange,
+  onRowChange
 }: {
   stackRef: RefObject<HTMLDivElement>;
   item: string;
-  onIndexChange: (item: string, i: number) => void;
+  onColumnChange: (item: string, i: number) => void;
+  onRowChange: (item: string, rowOffset: number) => void;
 }) => {
-  const [pos, setPos] = useState({ x: 0, y: 0 });
   const nodeRef = useRef(null);
   return (
     <Draggable
       nodeRef={nodeRef}
       handle=".drag-handle"
-      position={pos}
-      onStop={(_, { x }) => {
-        const { width } = getColumnWidthAndHeight(stackRef);
-        const xDiff = (x + 16) / width;
-        const i = Math.abs(xDiff) > xThreshold ? Math.sign(xDiff) * Math.ceil(Math.abs(xDiff) - xThreshold) : 0;
-        if (i === 0) {
-          setPos({ x: 0, y: 0 });
+      position={{ x: 0, y: 0 }}
+      onStop={(_, { x, node }) => {
+        const columnOffset = getColumnOffset(stackRef, x);
+        if (columnOffset === 0) {
+          onRowChange(item, getRowOffset(stackRef, node));
         } else {
-          onIndexChange(item, i);
+          onColumnChange(item, columnOffset);
         }
       }}
     >
@@ -62,16 +81,24 @@ const KanbanCard = ({
 
 const KanbanColumn = ({
   list,
-  onIndexChange
+  onColumnChange,
+  onRowChange
 }: {
   list: string[];
-  onIndexChange: (item: string, i: number) => void;
+  onColumnChange: (item: string, i: number) => void;
+  onRowChange: (item: string, rowOffset: number) => void;
 }) => {
   const stackRef = useRef<HTMLDivElement>(null);
   return (
-    <Stack ref={stackRef} sx={{ flex: 1, p: 2 }}>
+    <Stack ref={stackRef} sx={{ flex: 1, p: padding + "px" }}>
       {list.map((item) => (
-        <KanbanCard key={item} stackRef={stackRef} item={item} onIndexChange={onIndexChange} />
+        <KanbanCard
+          key={item}
+          stackRef={stackRef}
+          item={item}
+          onColumnChange={onColumnChange}
+          onRowChange={onRowChange}
+        />
       ))}
     </Stack>
   );
@@ -81,23 +108,39 @@ export const KanbanLayout = () => {
   const [columns, setColumns] = useState(data);
   return (
     <Stack sx={{ flex: 1, flexDirection: "row" }}>
-      {columns.map(({ name, list }, columnIndex) => (
+      {columns.map(({ name, list }, i) => (
         <Fragment key={name}>
-          {columnIndex !== 0 && <Divider orientation="vertical" />}
+          {i !== 0 && <Divider orientation="vertical" />}
           <KanbanColumn
             list={list}
-            onIndexChange={(selectedItem, j) => {
+            onColumnChange={(selectedItem, columnOffset) => {
               setColumns((prev) => {
                 const newColumns = [...prev];
-                newColumns[columnIndex].list = newColumns[columnIndex].list.filter((item) => item !== selectedItem);
-                let k = columnIndex + j;
-                if (k < 0) {
-                  k = 0;
+                newColumns[i].list = newColumns[i].list.filter((item) => item !== selectedItem);
+                let j = i + columnOffset;
+                if (j < 0) {
+                  j = 0;
                 }
-                if (k >= newColumns.length) {
-                  k = newColumns.length - 1;
+                if (j >= newColumns.length) {
+                  j = newColumns.length - 1;
                 }
-                newColumns[k].list = [...newColumns[k].list, selectedItem];
+                newColumns[j].list = [...newColumns[j].list, selectedItem];
+                return newColumns;
+              });
+            }}
+            onRowChange={(selectedItem, rowOffset) => {
+              setColumns((prev) => {
+                const newColumns = [...prev];
+                const column = newColumns[i];
+                column.list.splice(column.list.indexOf(selectedItem), 1);
+                let j = rowOffset;
+                if (j < 0) {
+                  j = 0;
+                }
+                if (j > column.list.length) {
+                  j = column.list.length;
+                }
+                column.list.splice(j, 0, selectedItem);
                 return newColumns;
               });
             }}
